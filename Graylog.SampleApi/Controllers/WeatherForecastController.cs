c5700f8fae2750643f8e8b6262f5923d98c69186
Beytullah Taşkın <beytullah.taskin@birlesikodeme.com>
2021-10-11 15:01:19 +0300

    .

--- Graylog.SampleApi/Controllers/WeatherForecastController.cs
+++ Graylog.SampleApi/Controllers/WeatherForecastController.cs
@@ -3,8 +3,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Sockets;
 using System.Threading;
 
 namespace Graylog.SampleApi.Controllers
